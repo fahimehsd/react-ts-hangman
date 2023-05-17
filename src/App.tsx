943e1a4b6864f9230d4e389df06f5a07f5d5d9c6
fahimehsd <fahimehsadeghi3@gmail.com>
2023-05-17 19:20:24 +0330

    keyboard and importing done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import words from "./wordList.json";
 import HangmanDrawing from "./HangmanDrawing";
+import HangmanWord from "./HangmanWord";
+import Keyboard from "./Keyboard";
 function App() {
   const [word, setWord] = useState(() => {
     return words[Math.floor(Math.random() * words.length)];
@@ -21,6 +23,8 @@ function App() {
     >
       <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
       <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
     </div>
   );
 }
